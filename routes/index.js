83afd7c2c4aa9ab5abc0440c9640301471de0123
doramong0926 <doramong0926@gmail.com>
2018-11-04 03:49:38 +0900

    version up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Blcscan' });
+  res.render('index', { title: 'Blcscan', version: '0.9.0' });
 });
 
 module.exports = router;
